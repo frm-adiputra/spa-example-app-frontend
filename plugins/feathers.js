6e04c7edadc3b16e2117efb0b4d8d6d52185b6b6
Firmansyah Adiputra <frm.adiputra@gmail.com>
2022-10-14 14:54:16 +0700

    chore: clean up

--- plugins/feathers.js
+++ plugins/feathers.js
@@ -1,34 +1,18 @@
 import feathers from '@feathersjs/feathers'
 import rest from '@feathersjs/rest-client'
 import axios from 'axios'
-// import socketio from '@feathersjs/socketio-client'
 import auth from '@feathersjs/authentication-client'
-// import io from 'socket.io-client'
-// import { CookieStorage } from 'cookie-storage'
-// import { iff, discard } from 'feathers-hooks-common'
 import feathersVuex, { initAuth, hydrateApi } from 'feathers-vuex'
-// const { iff, discard } = require('feathers-hooks-common')
 
 // Get the api url from the environment variable
 const apiUrl = process.env.API_URL
 const restClient = rest(apiUrl)
 const transport = restClient.axios(axios)
-// const storage = new CookieStorage()
 const storage = window.localStorage
 
 const feathersClient = feathers()
   .configure(transport)
   .configure(auth({ storage }))
-// .hooks({
-//   before: {
-//     all: [
-//       iff(
-//         (context) => ['create', 'update', 'patch'].includes(context.method),
-//         discard('__id', '__isTemp')
-//       ),
-//     ],
-//   },
-// })
 
 export default feathersClient
 
